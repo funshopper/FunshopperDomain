63a37f47cc4f53ff0ea74bd9062f7b2ae7f06871
Deepak RP <deepu1002@gmail.com>
2019-07-21 14:20:28 -0400

    Added swagger ui

--- src/main/java/com/funshopper/FunshopperDomain/Application.java
+++ src/main/java/com/funshopper/FunshopperDomain/Application.java
@@ -2,7 +2,9 @@ package com.funshopper.FunshopperDomain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
