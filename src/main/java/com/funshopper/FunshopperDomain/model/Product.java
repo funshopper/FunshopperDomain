0be5289f204c9f1d98927f9b311b03ddc9998416
Deepak RP <deepu1002@gmail.com>
2019-07-21 14:48:06 -0400

    Added gradle

--- src/main/java/com/funshopper/FunshopperDomain/model/Product.java
+++ src/main/java/com/funshopper/FunshopperDomain/model/Product.java
@@ -23,8 +23,7 @@ public class Product {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
-	
+	private Integer id;
 	private String name;
 	private String description;
 	private double price;
